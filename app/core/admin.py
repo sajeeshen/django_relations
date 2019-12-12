47f2d46409cbad943b56be673284fd233dabf4c4
Sajeesh E Namboothiri <sajeesh.namboothiri@awok.com>
2019-12-12 18:51:42 +0530

    Fixed bugs

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from core.models import Place, Restaurant
 # Register your models here.
+
+admin.site.register(Place)
+admin.site.register(Restaurant)
--- app/core/models.py
+++ app/core/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 # Create your models here.
-FOOD_CHOICES = ('Fast Food',
-                'Bar Attached',
-                'Bakery')
+FOOD_CHOICES = (('Fast Food', 'Fast Food'),
+                ('Bar Attached', 'Bar Attached'),
+                ('Bakery','Bakery'),('Normal', 'Normal'))
 
 class Place(models.Model):
     """ Model for place """
@@ -20,7 +20,7 @@ class Restaurant(models.Model):
     """ Restaurent model """
     place = models.OneToOneField(Place, on_delete=models.CASCADE, primary_key=True)
     name = models.CharField(max_length=50)
-
+    food_choices = models.CharField(choices=FOOD_CHOICES, default='Normal', max_length=50)
 
     def __str__(self):
         return self.name
